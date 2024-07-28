5fd1d5f7b0b16451b1afcd65ef7403a349334536
Syntactic Newman <146788478+syntacticnewman@users.noreply.github.com>
2024-07-29 00:25:09 +0200

    feat(QuackCounter): add QuackCounter decorator

---
+++ src/chapter-12/decorator/QuackCounter.ts
@@ -0,0 +1,19 @@
+import Quackable from '../interface/Quackable';
+
+export default class QuackCounter implements Quackable {
+  duck: Quackable;
+  static numberOfQuacks: number = 0;
+
+  constructor(duck: Quackable) {
+    this.duck = duck;
+  }
+
+  public quack(): void {
+    this.duck.quack();
+    QuackCounter.numberOfQuacks++;
+  }
+
+  public static getQuacks(): number {
+    return QuackCounter.numberOfQuacks;
+  }
+}
