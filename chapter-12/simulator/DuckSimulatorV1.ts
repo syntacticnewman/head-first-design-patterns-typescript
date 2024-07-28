f1b6900133b3cecdbe88cbe35aeca24e02051b3d
Syntactic Newman <146788478+syntacticnewman@users.noreply.github.com>
2024-07-28 02:46:35 +0200

    feat(simulator): DuckSimulatorV1

---
+++ chapter-12/simulator/DuckSimulatorV1.ts
@@ -0,0 +1,36 @@
+import DuckCall from "../class/DuckCall";
+import MallardDuck from "../class/MallardDuck";
+import RedheadDuck from "../class/RedheadDuck";
+import RubberDuck from "../class/RubberDuck";
+import Quackable from "../interface/Quackable";
+
+export default class DuckSimulatorV1 {
+  public static main(): void {
+    const simulator = new DuckSimulatorV1();
+    simulator.simulate();
+  }
+
+  simulate(): void;
+  simulate(duck: Quackable): void;
+
+  simulate(duck?: Quackable) {
+    if (!duck) {
+      const mallardDuck = new MallardDuck();
+      const redheadDuck = new RedheadDuck();
+      const duckCall = new DuckCall();
+      const rubberDuck = new RubberDuck();
+
+      console.log("Duck Simulator");
+
+      this.simulate(mallardDuck);
+      this.simulate(redheadDuck);
+      this.simulate(duckCall);
+      this.simulate(rubberDuck);
+    } else {
+      duck.quack();
+    }
+  }
+}
+
+// $ npx ts-node DuckSimulatorV1
+if (require.main === module) DuckSimulatorV1.main();
