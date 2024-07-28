5a44abd4c128af144c14967e9b177caf4eb53e09
Syntactic Newman <146788478+syntacticnewman@users.noreply.github.com>
2024-07-28 23:21:35 +0200

    refactor(DuckSimulator): implement GooseAdapter
    Simulator can now handle goose by using an adapter that implements the Quackable interface.
    
    In addition, type Quackable for the duck variables has been added to resemble the code in the book. Same for the simulator with the DuckSimulator type.

--- src/chapter-12/simulator/DuckSimulator.ts
+++ src/chapter-12/simulator/DuckSimulator.ts
@@ -1,4 +1,6 @@
 import DuckCall from '../class/DuckCall';
+import Goose from '../class/Goose';
+import GooseAdapter from '../adapter/GooseAdapter';
 import MallardDuck from '../class/MallardDuck';
 import Quackable from '../interface/Quackable';
 import RedheadDuck from '../class/RedheadDuck';
@@ -6,7 +8,7 @@ import RubberDuck from '../class/RubberDuck';
 
 export default class DuckSimulator {
   public static main(): void {
-    const simulator = new DuckSimulator();
+    const simulator: DuckSimulator = new DuckSimulator();
     simulator.simulate();
   }
 
@@ -15,17 +17,19 @@ export default class DuckSimulator {
 
   simulate(duck?: Quackable) {
     if (!duck) {
-      const mallardDuck = new MallardDuck();
-      const redheadDuck = new RedheadDuck();
-      const duckCall = new DuckCall();
-      const rubberDuck = new RubberDuck();
+      const mallardDuck: Quackable = new MallardDuck();
+      const redheadDuck: Quackable = new RedheadDuck();
+      const duckCall: Quackable = new DuckCall();
+      const rubberDuck: Quackable = new RubberDuck();
+      const gooseDuck: Quackable = new GooseAdapter(new Goose());
 
-      console.log('Duck Simulator');
+      console.log('Duck Simulator: With Goose Adapter');
 
       this.simulate(mallardDuck);
       this.simulate(redheadDuck);
       this.simulate(duckCall);
       this.simulate(rubberDuck);
+      this.simulate(gooseDuck);
     } else {
       duck.quack();
     }
