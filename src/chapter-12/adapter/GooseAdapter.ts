df1faf1e4be5dd23690fdb6b0cdccba143fbada6
Syntactic Newman <146788478+syntacticnewman@users.noreply.github.com>
2024-07-28 23:00:34 +0200

    feat(GooseAdapter): add GooseAdapter

---
+++ src/chapter-12/adapter/GooseAdapter.ts
@@ -0,0 +1,14 @@
+import Goose from '../class/Goose';
+import Quackable from '../interface/Quackable';
+
+export default class GooseAdapter implements Quackable {
+  public goose: Goose;
+
+  constructor(goose: Goose) {
+    this.goose = goose;
+  }
+
+  public quack(): void {
+    this.goose.honk();
+  }
+}
